c5f4beaef90fa8c70b94c58970197ef0ca8b991c
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 14:18:42 +0300

    Refactored class "StudentOrderValidator".

--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -14,7 +14,8 @@ public class StudentOrderValidator {
                 AnswerCityRegister cityAnswer = checkCityRegister(so);
                 if (!cityAnswer.success) {
                     //
-                    continue;
+                    //continue;
+                    break;
                 }
 
                 AnswerWedding wedAnswer = checkWedding(so);
@@ -23,32 +24,45 @@ public class StudentOrderValidator {
 
                 sendMail(so);
             }
-            System.out.println("Finish 2");
         }
 
         static StudentOrder readStudentOrder() {
             StudentOrder so = new StudentOrder();
-            return null;
+            return so;
         }
 
         static AnswerCityRegister checkCityRegister(StudentOrder so) {
-            return CityRegisterValidator.checkCityRegister(so);
+            CityRegisterValidator crv1 = new CityRegisterValidator();
+            crv1.hostName = "Host1";
+            crv1.login = "Login1";
+            crv1.password = "Pass1";
+
+            CityRegisterValidator crv2 = new CityRegisterValidator();
+            crv2.hostName = "Host2";
+            crv2.login = "Login2";
+            crv2.password = "Pass2";
+            AnswerCityRegister ans1 = crv1.checkCityRegister(so);
+            AnswerCityRegister ans2 = crv2.checkCityRegister(so);
+            return ans1;
         }
 
         static AnswerWedding checkWedding(StudentOrder so) {
+            WeddingValidator wd = new WeddingValidator();
             return WeddingValidator.checkWedding(so);
         }
 
         static AnswerChildren checkChildren(StudentOrder so) {
+            ChildrenValidator cv = new ChildrenValidator();
             return ChildrenValidator.checkChildren(so);
         }
 
         static AnswerStudent checkStudent(StudentOrder so) {
+            StudentValidator sv = new StudentValidator();
             return StudentValidator.checkStudent(so);
         }
 
         static void sendMail(StudentOrder so) {
-            System.out.println("Mail was sended.");
+            new MailSender().sendMail(so);
         }
     }
 }
