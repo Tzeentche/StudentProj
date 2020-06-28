0bab798cce8ab550f8d709719550c69f6afb4c99
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 13:46:18 +0300

    Add and fill class "ChildrenValidator".

---
+++ src/StudentValidator.java
@@ -0,0 +1,7 @@
+public class StudentValidator {
+
+    static AnswerStudent checkStudent(StudentOrder so) {
+        System.out.println("Student check is running");
+        return new AnswerStudent();
+    }
+}
