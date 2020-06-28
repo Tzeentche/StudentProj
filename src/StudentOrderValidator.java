2d324b273ebb06228586c1b2a36f5a196cc0b076
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 13:05:30 +0300

    Fill class "StudentOrderValidator".

--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -1,2 +1,60 @@
 public class StudentOrderValidator {
+
+    public static void main(String[] args) {
+        static void checkAll() {
+            while(true) {
+                StudentOrder so = readStudentOrder();
+                System.out.println("Start");
+                if (so == null) {
+                    break;
+                }
+
+                System.out.println("Finish");
+
+                AnswerCityRegister cityAnswer = checkCityRegister(so);
+                if (!cityAnswer.success) {
+                    //
+                    continue;
+                }
+
+                AnswerWedding wedAnswer = checkWedding(so);
+                AnswerChildren childrenAnswer = checkChildren(so);
+                AnswerStudent studentAnswer = checkStudent(so);
+
+                sendMail(so);
+            }
+            System.out.println("Finish 2");
+        }
+
+        static StudentOrder readStudentOrder() {
+            StudentOrder so = new StudentOrder();
+            return null;
+        }
+
+        static AnswerCityRegister checkCityRegister(StudentOrder so) {
+            System.out.println("CityRegister check is running");
+            AnswerCityRegister ans = new AnswerCityRegister();
+            ans.success = false;
+            return ans;
+        }
+
+        static AnswerWedding checkWedding(StudentOrder so) {
+            System.out.println("Wedding check is running");
+            return new AnswerWedding();
+        }
+
+        static AnswerChildren checkChildren(StudentOrder so) {
+            System.out.println("Children check is running");
+            return new AnswerChildren();
+        }
+
+        static AnswerStudent checkStudent(StudentOrder so) {
+            System.out.println("Student check is running");
+            return new AnswerStudent();
+        }
+
+        static void sendMail(StudentOrder so) {
+            System.out.println("Mail was sended.");
+        }
+    }
 }
