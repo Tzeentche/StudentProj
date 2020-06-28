3803008e1cc4dbc36ffdc077e6866c0dc59b269f
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 13:44:34 +0300

    Fill class "ChildrenValidator".

--- src/ChildrenValidator.java
+++ src/ChildrenValidator.java
@@ -1,2 +1,7 @@
 public class ChildrenValidator {
+
+    static AnswerChildren checkChildren(StudentOrder so) {
+        System.out.println("Children check is running");
+        return new AnswerChildren();
+    }
 }
