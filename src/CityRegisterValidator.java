3c708d74ba0eede02bbaf594b02fd0cd49591bec
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 13:06:44 +0300

    Add method to class "CityRegisterValidator".

--- src/CityRegisterValidator.java
+++ src/CityRegisterValidator.java
@@ -1,4 +1,9 @@
 public class CityRegisterValidator {
 
-    
+    static AnswerCityRegister checkCityRegister(StudentOrder so) {
+        System.out.println("CityRegister check is running");
+        AnswerCityRegister ans = new AnswerCityRegister();
+        ans.success = false;
+        return ans;
+    }
 }
