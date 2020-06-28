fa76844567ca451447bc1467157a95ca6b739315
Tzeentche <ilyasugako87@gmail.com>
2020-06-28 14:21:16 +0300

    Refactored class "CityRegisterValidator".

--- src/CityRegisterValidator.java
+++ src/CityRegisterValidator.java
@@ -1,7 +1,11 @@
 public class CityRegisterValidator {
 
-    static AnswerCityRegister checkCityRegister(StudentOrder so) {
-        System.out.println("CityRegister check is running");
+    String hostName;
+    String login;
+    String password;
+
+    AnswerCityRegister checkCityRegister(StudentOrder so) {
+        System.out.println("CityRegister check is running:" + hostName + "," + login + "," + password);
         AnswerCityRegister ans = new AnswerCityRegister();
         ans.success = false;
         return ans;
