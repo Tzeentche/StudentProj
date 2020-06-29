925d02e2576a50b808ce434316f877f9028dc0ce
Tzeentche <ilyasugako87@gmail.com>
2020-06-29 23:38:17 +0300

    Removed class "StudentValidator".

--- src/StudentValidator.java
+++
@@ -1,7 +0,0 @@
-public class StudentValidator {
-
-    AnswerStudent checkStudent(StudentOrder so) {
-        System.out.println("Student check is running");
-        return new AnswerStudent();
-    }
-}
