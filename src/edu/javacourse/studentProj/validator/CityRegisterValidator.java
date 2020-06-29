4bea171333d51834fed497b8b940951ca51395fe
Tzeentche <ilyasugako87@gmail.com>
2020-06-29 23:38:01 +0300

    Removed class "CityRegisterValidator".

--- src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
@@ -1,10 +1,15 @@
+package edu.javacourse.studentProj.validator;
+
+import edu.javacourse.studentProj.domain.AnswerCityRegister;
+import edu.javacourse.studentProj.domain.StudentOrder;
+
 public class CityRegisterValidator {
 
     String hostName;
     String login;
     String password;
 
-    AnswerCityRegister checkCityRegister(StudentOrder so) {
+    public AnswerCityRegister checkCityRegister(StudentOrder so) {
         System.out.println("CityRegister check is running:" + hostName + "," + login + "," + password);
         AnswerCityRegister ans = new AnswerCityRegister();
         ans.success = false;
