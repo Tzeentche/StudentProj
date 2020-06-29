6ce4fdf77561443472d40300e75a926f35e757df
Tzeentche <ilyasugako87@gmail.com>
2020-06-29 23:38:35 +0300

    Removed class "WeddingValidator".

--- src/WeddingValidator.java
+++
@@ -1,7 +0,0 @@
-public class WeddingValidator {
-
-    AnswerWedding checkWedding(StudentOrder so) {
-        System.out.println("Wedding check is running");
-        return new AnswerWedding();
-    }
-}
