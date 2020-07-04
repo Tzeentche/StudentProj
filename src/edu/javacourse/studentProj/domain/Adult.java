e6fe8b1aa65b7467cd5e4ebb7b07b813f50c346c
Tzeentche <ilyasugako87@gmail.com>
2020-07-04 23:41:25 +0300

    Refactored class "Adult".

--- src/edu/javacourse/studentProj/domain/Adult.java
+++ src/edu/javacourse/studentProj/domain/Adult.java
@@ -11,6 +11,14 @@ public class Adult extends Person {
     private String university;
     private String studentId;
 
+    public Adult(String surName, String givenName, String patronymic, LocalDate dateOfBirth) {
+        super(surName, givenName, patronymic, dateOfBirth);
+    }
+
+        public String getSurname() {
+            return "";
+        }
+
     public String getAdultString() {
         return getSurName() + " " + getGivenName() + " " + pasportSeria;
     }
