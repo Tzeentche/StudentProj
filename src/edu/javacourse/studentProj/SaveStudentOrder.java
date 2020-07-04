1d92c2a748122ab6ad75ed5e0e788b86a5887850
Tzeentche <ilyasugako87@gmail.com>
2020-07-04 23:42:03 +0300

    Refactored class "SaveStudentOrder".

--- src/edu/javacourse/studentProj/SaveStudentOrder.java
+++ src/edu/javacourse/studentProj/SaveStudentOrder.java
@@ -23,6 +23,8 @@ public class SaveStudentOrder {
     public static StudentOrder buildStudentOrder(long id) {
         StudentOrder so = new StudentOrder();
         so.setStudentOrderId(id);
+
+        Adult husband = new Adult("", "", "", null);
         return so;
     }
 }
