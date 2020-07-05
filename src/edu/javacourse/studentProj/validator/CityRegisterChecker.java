11169590a2ee0d80511310c9ca1b233c98f1743a
Tzeentche <ilyasugako87@gmail.com>
2020-07-05 15:22:58 +0300

    Refactored class "CityRegisterChecker".

--- src/edu/javacourse/studentProj/validator/CityRegisterChecker.java
+++ src/edu/javacourse/studentProj/validator/CityRegisterChecker.java
@@ -2,8 +2,9 @@ package edu.javacourse.studentProj.validator;
 
 import edu.javacourse.studentProj.domain.CityRegisterCheckerResponse;
 import edu.javacourse.studentProj.domain.Person;
+import edu.javacourse.studentProj.exception.CityRegisterException;
 
 public interface CityRegisterChecker {
 
-    CityRegisterCheckerResponse checkPerson(Person person);
+    CityRegisterCheckerResponse checkPerson(Person person) throws CityRegisterException;
 }
