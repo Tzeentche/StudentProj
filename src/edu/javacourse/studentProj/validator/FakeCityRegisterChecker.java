fd4c0b87c35edc40cd657c636da669ca42a983bf
Tzeentche <ilyasugako87@gmail.com>
2020-07-05 12:34:35 +0300

    Create class "FakeCityRegisterChecker".

---
+++ src/edu/javacourse/studentProj/validator/FakeCityRegisterChecker.java
@@ -0,0 +1,12 @@
+package edu.javacourse.studentProj.validator;
+
+import edu.javacourse.studentProj.domain.CityRegisterCheckerResponse;
+import edu.javacourse.studentProj.domain.Person;
+
+public class FakeCityRegisterChecker {
+
+    public CityRegisterCheckerResponse  checkPerson(Person person) {
+
+        return null;
+    }
+}
