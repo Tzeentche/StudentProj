846d00d0189b9b41e47047553186441c762e38ea
Tzeentche <ilyasugako87@gmail.com>
2020-07-05 12:35:01 +0300

    Refactored class "CityRegisterValidator".

--- src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
@@ -1,15 +1,28 @@
 package edu.javacourse.studentProj.validator;
 
 import edu.javacourse.studentProj.domain.AnswerCityRegister;
+import edu.javacourse.studentProj.domain.Person;
 import edu.javacourse.studentProj.domain.StudentOrder;
 
 public class CityRegisterValidator {
 
-    String hostName;
-    String login;
+    public String hostName;
+    protected int port;
+    private String login;
     String password;
 
+    private FakeCityRegisterChecker personChecker;
+
+    public CityRegisterValidator(String hostName) {
+        personChecker = new FakeCityRegisterChecker();
+    }
+
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
+        Person p = so.getHusband();
+        personChecker.checkPerson(so.getHusband());
+        personChecker.checkPerson(so.getWife());
+        personChecker.checkPerson(so.getChild());
+
         System.out.println("CityRegister check is running:" + hostName + "," + login + "," + password);
         AnswerCityRegister ans = new AnswerCityRegister();
         ans.success = false;
