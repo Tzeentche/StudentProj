bb7d47aba727c8a4aeba38546cf1384ba152ba74
Tzeentche <ilyasugako87@gmail.com>
2020-07-07 21:31:54 +0300

    Refactored class CityRegisterValidator.

--- src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
@@ -1,9 +1,11 @@
 package edu.javacourse.studentProj.validator;
 
 import edu.javacourse.studentProj.domain.*;
+import edu.javacourse.studentProj.domain.register.AnswerCityRegister;
+import edu.javacourse.studentProj.domain.register.AnswerCityRegisterItem;
+import edu.javacourse.studentProj.domain.register.CityRegisterResponse;
 import edu.javacourse.studentProj.exception.CityRegisterException;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class CityRegisterValidator {
@@ -20,34 +22,27 @@ public class CityRegisterValidator {
     }
 
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
-        Person p = so.getHusband();
+        AnswerCityRegister ans = new AnswerCityRegister();
+
+        checkPerson(so.getHusband());
+        checkPerson(so.getWife());
+
+        for (Child child : so.getChildren()) {
+                checkPerson(child);
+            }
+
+        return ans;
+    }
+
+    private AnswerCityRegisterItem checkPerson(Person person) {
 
         try {
-            CityRegisterCheckerResponse hans = personChecker.checkPerson(so.getHusband());
-            CityRegisterCheckerResponse wans = personChecker.checkPerson(so.getWife());
-
-            List<Child> children = so.getChildren();
-
-            for(int i = 0; i < so.getChildren().size(); i++) {
-                CityRegisterCheckerResponse cans = personChecker.checkPerson(children.get(i));
-            }
-
-            for(Iterator<Child> it = children.iterator(); it.hasNext();) {
-                Child child = it.next();
-                CityRegisterCheckerResponse cans = personChecker.checkPerson(child);
-            }
-
-            for(Child child : children) {
-                CityRegisterCheckerResponse cans = personChecker.checkPerson(child);
-            }
+            CityRegisterResponse cans = personChecker.checkPerson(person);
 
         } catch (CityRegisterException ex) {
             ex.printStackTrace(System.out);
         }
 
-        System.out.println("CityRegister check is running:" + hostName + "," + login + "," + password);
-        AnswerCityRegister ans = new AnswerCityRegister();
-        ans.success = false;
-        return ans;
+        return null;
     }
 }
