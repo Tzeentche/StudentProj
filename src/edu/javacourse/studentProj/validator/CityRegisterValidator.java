7fb9dee44607927a444d795e638b617b36994d09
Tzeentche <ilyasugako87@gmail.com>
2020-07-07 22:02:53 +0300

    Refactored class CityRegisterValidator.

--- src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentProj/validator/CityRegisterValidator.java
@@ -5,16 +5,13 @@ import edu.javacourse.studentProj.domain.register.AnswerCityRegister;
 import edu.javacourse.studentProj.domain.register.AnswerCityRegisterItem;
 import edu.javacourse.studentProj.domain.register.CityRegisterResponse;
 import edu.javacourse.studentProj.exception.CityRegisterException;
+import edu.javacourse.studentProj.exception.TransportException;
 
 import java.util.List;
 
 public class CityRegisterValidator {
 
-    public String hostName;
-    protected int port;
-    private String login;
-    String password;
-
+    public static final String IN_CODE = "NO_GRN";
     private FakeCityRegisterChecker personChecker;
 
     public CityRegisterValidator(String hostName) {
@@ -24,11 +21,11 @@ public class CityRegisterValidator {
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
         AnswerCityRegister ans = new AnswerCityRegister();
 
-        checkPerson(so.getHusband());
-        checkPerson(so.getWife());
+        ans.addItem(checkPerson(so.getHusband()));
+        ans.addItem(checkPerson(so.getWife()));
 
         for (Child child : so.getChildren()) {
-                checkPerson(child);
+                ans.addItem(checkPerson(child));
             }
 
         return ans;
@@ -36,13 +33,24 @@ public class CityRegisterValidator {
 
     private AnswerCityRegisterItem checkPerson(Person person) {
 
+        AnswerCityRegisterItem.CityStatus status = null;
+        AnswerCityRegisterItem.CityError error = null;
+
         try {
-            CityRegisterResponse cans = personChecker.checkPerson(person);
+            CityRegisterResponse tmp = personChecker.checkPerson(person);
+            status = tmp.isExisting() ? AnswerCityRegisterItem.CityStatus.YES : AnswerCityRegisterItem.CityStatus.NO;
 
         } catch (CityRegisterException ex) {
             ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(ex.getCode(), ex.getMessage());
+        } catch (TransportException ex) {
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
         }
 
+        AnswerCityRegisterItem and = new AnswerCityRegisterItem(status, person, error);
         return null;
     }
 }
