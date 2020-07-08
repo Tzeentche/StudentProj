1aa1cc0319da38ecb49952e8e7c0a4516164052a
Tzeentche <ilyasugako87@gmail.com>
2020-07-08 21:23:58 +0300

    Refactored class RealCityRegisterChecker.

--- src/edu/javacourse/studentProj/validator/RealCityRegisterChecker.java
+++ src/edu/javacourse/studentProj/validator/RealCityRegisterChecker.java
@@ -3,11 +3,11 @@ package edu.javacourse.studentProj.validator;
 import edu.javacourse.studentProj.domain.register.CityRegisterResponse;
 import edu.javacourse.studentProj.domain.Person;
 import edu.javacourse.studentProj.exception.CityRegisterException;
+import edu.javacourse.studentProj.exception.TransportException;
 
 public class RealCityRegisterChecker implements CityRegisterChecker{
 
-    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException {
-
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
 
         return null;
     }
