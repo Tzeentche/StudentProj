e7239e906f8dc2e8a8a88057013c09de780ef06b
Tzeentche <ilyasugako87@gmail.com>
2020-07-26 22:04:05 +0300

    Refactored class StudentOrder.

--- src/edu/javacourse/studentProj/domain/StudentOrder.java
+++ src/edu/javacourse/studentProj/domain/StudentOrder.java
@@ -1,48 +1,37 @@
 package edu.javacourse.studentProj.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class StudentOrder {
 
     private long studentOrderId;
+//    private StudentOrderStatus studentOrderStatus;
+    private LocalDateTime studentOrderDate;
     private Adult husband;
     private Adult wife;
     private List<Child> children;
     private String marriageCertificateId;
-    private String marriageOffice;
+//    private RegisterOffice marriageOffice;
     private LocalDate marriageDate;
 
-    public void addChild(Child child) {
-        if(children == null) {
-            children = new ArrayList<>(5);
-        }
-        children.add(child);
+//    public StudentOrderStatus getStudentOrderStatus() {
+//        return studentOrderStatus;
+//    }
+//
+//    public void setStudentOrderStatus(StudentOrderStatus studentOrderStatus) {
+//        this.studentOrderStatus = studentOrderStatus;
+//    }
+
+    public LocalDateTime getStudentOrderDate() {
+        return studentOrderDate;
     }
 
-    public String getMarriageCertificateId() {
-        return marriageCertificateId;
-    }
-
-    public void setMarriageCertificateId(String marriageCertificateId) {
-        this.marriageCertificateId = marriageCertificateId;
-    }
-
-    public String getMarriageOffice() {
-        return marriageOffice;
-    }
-
-    public void setMarriageOffice(String marriageOffice) {
-        this.marriageOffice = marriageOffice;
-    }
-
-    public LocalDate getMarriageDate() {
-        return marriageDate;
-    }
-
-    public void setMarriageDate(LocalDate marriageDate) {
-        this.marriageDate = marriageDate;
+    public void setStudentOrderDate(LocalDateTime studentOrderDate) {
+        this.studentOrderDate = studentOrderDate;
     }
 
     public long getStudentOrderId() {
@@ -69,11 +58,38 @@ public class StudentOrder {
         this.wife = wife;
     }
 
-//    public Child getChild() {
-//        return children;
+    public void addChild(Child child) {
+        if (children == null) {
+            children = new ArrayList<>(5);
+        }
+        children.add(child);
+    }
+
+    public List<Child> getChildren() {
+        return children;
+    }
+
+    public String getMarriageCertificateId() {
+        return marriageCertificateId;
+    }
+
+    public void setMarriageCertificateId(String marriageCertificateId) {
+        this.marriageCertificateId = marriageCertificateId;
+    }
+
+//    public RegisterOffice getMarriageOffice() {
+//        return marriageOffice;
 //    }
 //
-//    public void setChild(Child child) {
-//        this.children = children;
+//    public void setMarriageOffice(RegisterOffice marriageOffice) {
+//        this.marriageOffice = marriageOffice;
 //    }
+
+    public LocalDate getMarriageDate() {
+        return marriageDate;
+    }
+
+    public void setMarriageDate(LocalDate marriageDate) {
+        this.marriageDate = marriageDate;
+    }
 }
