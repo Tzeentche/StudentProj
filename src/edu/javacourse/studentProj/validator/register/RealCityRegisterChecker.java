636336dfa2c31d38823c8d2d0eade24989acaba5
Tzeentche <ilyasugako87@gmail.com>
2021-01-15 23:34:45 +0300

    Replaced class RealCityRegisterChecker.

--- src/edu/javacourse/studentProj/validator/register/RealCityRegisterChecker.java
+++ src/edu/javacourse/studentProj/validator/register/RealCityRegisterChecker.java
@@ -1,11 +1,12 @@
-package edu.javacourse.studentProj.validator;
+package edu.javacourse.studentProj.validator.register;
 
+import edu.javacourse.studentProj.validator.register.CityRegisterChecker;
 import edu.javacourse.studentProj.validator.register.CityRegisterResponse;
 import edu.javacourse.studentProj.domain.Person;
 import edu.javacourse.studentProj.exception.CityRegisterException;
 import edu.javacourse.studentProj.exception.TransportException;
 
-public class RealCityRegisterChecker implements CityRegisterChecker{
+public class RealCityRegisterChecker implements CityRegisterChecker {
 
     public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
 
