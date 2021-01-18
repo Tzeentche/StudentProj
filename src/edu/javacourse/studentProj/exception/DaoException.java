40cf0ebe645215b854b7a62d17e9d0f7a05e74be
Tzeentche <ilyasugako87@gmail.com>
2021-01-19 00:17:47 +0300

    Refactored DaoException class.

--- src/edu/javacourse/studentProj/exception/DaoException.java
+++ src/edu/javacourse/studentProj/exception/DaoException.java
@@ -2,5 +2,14 @@ package edu.javacourse.studentProj.exception;
 
 public class DaoException extends Exception {
 
-    
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
