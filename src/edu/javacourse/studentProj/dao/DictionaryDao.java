2563fceac43f119fe96033b4ffd8e847b9bc1408
Tzeentche <ilyasugako87@gmail.com>
2021-01-19 23:49:55 +0300

    Refactored DictionaryDao class.

--- src/edu/javacourse/studentProj/dao/DictionaryDao.java
+++ src/edu/javacourse/studentProj/dao/DictionaryDao.java
@@ -1,10 +1,11 @@
 package edu.javacourse.studentProj.dao;
 
 import edu.javacourse.studentProj.domain.Street;
+import edu.javacourse.studentProj.exception.DaoException;
 
 import java.util.List;
 
 public interface DictionaryDao {
 
-    List<Street> findStreet(String pattern)
+    List<Street> findStreets(String pattern) throws DaoException;
 }
