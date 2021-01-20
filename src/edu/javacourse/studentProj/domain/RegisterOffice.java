276d53feb0d19f370a19693c6cbb7b937d994c11
Tzeentche <ilyasugako87@gmail.com>
2021-01-20 23:53:59 +0300

    Refactored RegisterOffice class.

--- src/edu/javacourse/studentProj/domain/RegisterOffice.java
+++ src/edu/javacourse/studentProj/domain/RegisterOffice.java
@@ -1,4 +1,42 @@
 package edu.javacourse.studentProj.domain;
 
 public class RegisterOffice {
+
+    private long office_id;
+    private String officeAreaId;
+    private String officeName;
+
+
+    public RegisterOffice() {
+    }
+
+    public RegisterOffice(long office_id, String officeAreaId, String officeName) {
+        this.office_id = office_id;
+        this.officeAreaId = officeAreaId;
+        this.officeName = officeName;
+    }
+
+    public long getOffice_id() {
+        return office_id;
+    }
+
+    public void setOffice_id(long office_id) {
+        this.office_id = office_id;
+    }
+
+    public String getOfficeAreaId() {
+        return officeAreaId;
+    }
+
+    public void setOfficeAreaId(String officeAreaId) {
+        this.officeAreaId = officeAreaId;
+    }
+
+    public String getOfficeName() {
+        return officeName;
+    }
+
+    public void setOfficeName(String officeName) {
+        this.officeName = officeName;
+    }
 }
