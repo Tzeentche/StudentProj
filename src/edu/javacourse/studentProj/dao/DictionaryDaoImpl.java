60b701b77230603cf89bf502f107be83cb096a12
Tzeentche <ilyasugako87@gmail.com>
2021-01-20 23:54:52 +0300

    Refactored DictionaryDaoImpl class.

--- src/edu/javacourse/studentProj/dao/DictionaryDaoImpl.java
+++ src/edu/javacourse/studentProj/dao/DictionaryDaoImpl.java
@@ -1,6 +1,8 @@
 package edu.javacourse.studentProj.dao;
 
 import edu.javacourse.studentProj.config.Config;
+import edu.javacourse.studentProj.domain.PassportOffice;
+import edu.javacourse.studentProj.domain.RegisterOffice;
 import edu.javacourse.studentProj.domain.Street;
 import edu.javacourse.studentProj.exception.DaoException;
 
@@ -11,6 +13,12 @@ public class DictionaryDaoImpl implements DictionaryDao {
 
     private static final String GET_STREET = "SELECT street_code, street_name " +
             "FROM jc_streets WHERE UPPER(street_name) LIKE UPPER (?)";
+
+    private static final String GET_PASSPORT = "SELECT * " +
+            "FROM jc_passport_office WHERE p_office_area_id = ?";
+
+    private static final String GET_REGISTER = "SELECT * " +
+            "FROM jc_register_office WHERE r_office_area_id = ?";
 
     private Connection getConnection() throws SQLException {
         Connection con = DriverManager.getConnection(
@@ -32,6 +40,50 @@ public class DictionaryDaoImpl implements DictionaryDao {
                 while (rs.next()) {
                     Street str = new Street(rs.getLong("street_code"), rs.getString("street_name"));
                     result.add(str);
+            }
+        }catch (SQLException ex) {
+            throw new DaoException(ex);
+        }
+        return result;
+    }
+
+    @Override
+    public List<PassportOffice> findPassportOffices(String areaId) throws DaoException {
+        List<PassportOffice> result = new LinkedList<>();
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(GET_PASSPORT)) {
+
+            stmt.setString(1, areaId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                PassportOffice str = new PassportOffice(
+                        rs.getLong("p_office_id"),
+                        rs.getString("p_office_area_id"),
+                        rs.getString("p_office_name"));
+                result.add(str);
+            }
+        }catch (SQLException ex) {
+            throw new DaoException(ex);
+        }
+        return result;
+    }
+
+    @Override
+    public List<RegisterOffice> findRegisterOffices(String areaId) throws DaoException {
+        List<RegisterOffice> result = new LinkedList<>();
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(GET_REGISTER)) {
+
+            stmt.setString(1, areaId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                RegisterOffice str = new RegisterOffice(
+                        rs.getLong("r_office_id"),
+                        rs.getString("r_office_area_id"),
+                        rs.getString("r_office_name"));
+                result.add(str);
             }
         }catch (SQLException ex) {
             throw new DaoException(ex);
