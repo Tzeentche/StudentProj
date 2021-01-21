a0ac09de1da26c0ebb2f9257db86fd99d92ca544
Tzeentche <ilyasugako87@gmail.com>
2021-01-21 23:08:17 +0300

    Refactored StudentOrderDao class.

--- src/edu/javacourse/studentProj/dao/StudentOrderDao.java
+++ src/edu/javacourse/studentProj/dao/StudentOrderDao.java
@@ -1,6 +1,9 @@
 package edu.javacourse.studentProj.dao;
 
+import edu.javacourse.studentProj.domain.StudentOrder;
+import edu.javacourse.studentProj.exception.DaoException;
+
 public interface StudentOrderDao {
 
-    
+    Long saveStudentOrder(StudentOrder so) throws DaoException;
 }
