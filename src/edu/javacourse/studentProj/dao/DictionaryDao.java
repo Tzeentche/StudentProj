2fd4f8791fdc2e840cf5fbde1b42e5f357699b56
Tzeentche <ilyasugako87@gmail.com>
2021-01-21 22:39:49 +0300

    Refactored DictionaryDao class.

--- src/edu/javacourse/studentProj/dao/DictionaryDao.java
+++ src/edu/javacourse/studentProj/dao/DictionaryDao.java
@@ -12,4 +12,5 @@ public interface DictionaryDao {
     List<Street> findStreets(String pattern) throws DaoException;
     List<PassportOffice> findPassportOffices(String areaId) throws DaoException;
     List<RegisterOffice> findRegisterOffices(String areaId) throws DaoException;
+    List<CountryArea> findAreas(String areaId) throws DaoException;
 }
