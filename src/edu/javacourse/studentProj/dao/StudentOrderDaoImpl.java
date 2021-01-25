45e7cb01c065ec41e715afb3e425ecd1b8fd9e1b
Tzeentche <ilyasugako87@gmail.com>
2021-01-25 22:57:00 +0300

    Refactored StudentOrderDaoImpl class.

--- src/edu/javacourse/studentProj/dao/StudentOrderDaoImpl.java
+++ src/edu/javacourse/studentProj/dao/StudentOrderDaoImpl.java
@@ -27,8 +27,14 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
     private static final String SELECT_ORDERS=
-        "SELECT so.*, ro.r_office_area_id, ro.r_office_name FROM jc_student_orders so " + " INNER JOIN jc_register_office ro ON " +
-                "ro.r_office_id = so.register_office_id " + "WHERE student_order_status = 0 ORDER BY student_order_date";
+        "SELECT so.*, ro.r_office_area_id, ro.r_office_name, " +
+                "po_h.p_officee_area_id as h_p_office_area_id, po_h.p_office_name as h_p_office_name, " +
+                "po_w.p_officee_area_id as w_p_office_area_id, po_w.p_office_name as w_p_office_name " +
+                "FROM jc_student_orders so " +
+                "INNER JOIN jc_register_office ro ON ro.r_office_id = so.register_office_id " +
+                "INNER JOIN jc_passport_office po_h ON po_h.p_office_id = so.h_passport_office_id " +
+                "INNER JOIN jc_passport_office po_w ON po_w.p_office_id = so.w_passport_office_id " +
+                "WHERE student_order_status = 0 ORDER BY student_order_date";
 
 //    TODO refactoring - make one method
     private Connection getConnection() throws SQLException {
@@ -183,7 +189,10 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
         adult.setPassportNumber(rs.getString(pref + "passport_number"));
         adult.setIssueDate(rs.getDate(pref + "passport_date").toLocalDate());
 
-        PassportOffice po = new PassportOffice(rs.getLong(pref + "passport_office_id"), "", "");
+        Long poId = rs.getLong(pref + "passport_office_id");
+        String poArea = rs.getString(pref + "p_office_area_id");
+        String poName = rs.getString(pref + "p_office_name");
+        PassportOffice po = new PassportOffice(poId, poArea, poName);
         adult.setIssueDepartment(po);
         Address adr = new Address();
         Street st = new Street(rs.getLong(pref + "street_code"), "");
