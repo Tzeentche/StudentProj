945f8334770a9b0bc6538bfe95d7148c16aaf36e
Tzeentche <ilyasugako87@gmail.com>
2021-01-27 23:51:06 +0300

    Refactored SimpleRunner class.

--- src/edu/javacourse/studentProj/dao/SimpleRunner.java
+++ src/edu/javacourse/studentProj/dao/SimpleRunner.java
@@ -7,5 +7,29 @@ import java.lang.reflect.Method;
 
 public class SimpleRunner {
 
-   
+    public static void main(String[] args) {
+        SimpleRunner sr =  new SimpleRunner();
+
+        sr.runTests();
+    }
+
+    private void runTests() {
+        try {
+            Class cl = Class.forName("edu.javacourse.studentProj.dao.DictionaryDaoImplTest");
+
+            Constructor cst = cl.getConstructor();
+            Object entity = cst.newInstance();
+
+            Method[] meth = cl.getMethods();
+            for(Method m : meth) {
+                Test ann = m.getAnnotation(Test.class);
+                if (ann != null) {
+                    m.invoke(entity);
+                }
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
