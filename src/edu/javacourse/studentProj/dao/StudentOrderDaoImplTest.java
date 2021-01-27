59f858e64441e09805aa51a56c5fb65f50a4b527
Tzeentche <ilyasugako87@gmail.com>
2021-01-27 23:58:42 +0300

    Refactored StudentOrderDaoImplTest class.

--- src/edu/javacourse/studentProj/dao/StudentOrderDaoImplTest.java
+++ src/edu/javacourse/studentProj/dao/StudentOrderDaoImplTest.java
@@ -1,8 +1,21 @@
 package edu.javacourse.studentProj.dao;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 class DictionaryDaoImplTest {
+
+    @BeforeClass
+    public static void startUp() {
+        System.out.println("START UP");
+    }
+
+    @Before
+    public void startTest() {
+        System.out.println("START TEST");
+    }
 
     @Test
     public void TestExample1() {
@@ -10,6 +23,7 @@ class DictionaryDaoImplTest {
     }
 
     @Test
+    @Ignore
     public void TestExample2() {
         System.out.println("TEST 2");
     }
@@ -17,6 +31,7 @@ class DictionaryDaoImplTest {
     @Test
     public void TestExample3() {
         System.out.println("TEST 3");
+        throw new RuntimeException("Bad Result");
     }
 
 }
