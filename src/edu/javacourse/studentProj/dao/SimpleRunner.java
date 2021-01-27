6eea4e396fd917d1bba0bd5c72519576dfac481d
Tzeentche <ilyasugako87@gmail.com>
2021-01-27 23:50:42 +0300

    Create SimpleRunner class.

---
+++ src/edu/javacourse/studentProj/dao/SimpleRunner.java
@@ -0,0 +1,11 @@
+package edu.javacourse.studentProj.dao;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class SimpleRunner {
+
+   
+}
