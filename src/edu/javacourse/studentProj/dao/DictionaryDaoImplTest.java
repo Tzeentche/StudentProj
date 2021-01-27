2f3419fb9067209ed1a5dc6eeec7b72534896a1f
Tzeentche <ilyasugako87@gmail.com>
2021-01-27 23:22:18 +0300

    Create DictionaryDaoImplTest class.

---
+++ src/edu/javacourse/studentProj/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,12 @@
+package edu.javacourse.studentProj.dao;
+
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void TestExample() {
+    System.out.println("TEST");
+}
+
+}
