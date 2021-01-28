7f6e4370e792e57e687a734b522bb3c3f709c459
Tzeentche <ilyasugako87@gmail.com>
2021-01-28 23:20:44 +0300

    Refactored StudentOrderDaoImplTest class.

--- src/edu/javacourse/studentProj/dao/StudentOrderDaoImplTest.java
+++ src/edu/javacourse/studentProj/dao/StudentOrderDaoImplTest.java
@@ -9,17 +9,17 @@ import java.net.ConnectException;
 import java.net.URL;
 import java.nio.file.*;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class DictionaryDaoImplTests {
-
+public class StudentOrderDaoImplTest {
 
     @BeforeClass
     public static void startUp() throws SQLException, IOException, ConnectException, Exception {
-        URL url1 = DictionaryDaoImplTests.class.getClassLoader()
+        URL url1 = StudentOrderDaoImplTest.class.getClassLoader()
                 .getResource("student_project.sql");
-        URL url2 = DictionaryDaoImplTests.class.getClassLoader()
+        URL url2 = StudentOrderDaoImplTest.class.getClassLoader()
                 .getResource("student_data.sql");
 
         List<String> str1 = Files.readAllLines(Paths.get(url1.toURI()));
@@ -66,6 +66,75 @@ class DictionaryDaoImplTests {
         Assert.assertTrue(ca4.size() == 2);
 
 
+    }
+
+    @Test
+    public void saveStudentOrder() throws DaoException {
+        StudentOrder so = buildStudentOrder(10);
+        Long id = new StudentOrderDaoImpl().saveStudentOrder(so);
+    }
+
+    @Test
+    public void getStudentOrder() {
+
+    }
+
+    public StudentOrder buildStudentOrder(long id) {
+        StudentOrder so = new StudentOrder();
+        so.setStudentOrderId(id);
+        so.setMarriageCertificateId("" + (123456000 + id));
+        so.setMarriageDate(LocalDate.of(2016, 7, 4));
+        RegisterOffice ro1 = new RegisterOffice(1L, " ", " ");
+        so.setMarriageOffice(ro1);
+
+        Street street = new Street(1L, "First str.");
+        Address address = new Address("195000", street, "12", "", "142");
+
+//        HUSBAND
+        Adult husband = new Adult("Петров", "Виктор", "Сергеевич", LocalDate.of(1997, 8, 24));
+        husband.setPassportSeria("" + (1000 + id));
+        husband.setPassportNumber("" + (100000 + id));
+        husband.setIssueDate(LocalDate.of(2017, 9, 15));
+        PassportOffice po1 = new PassportOffice(1L, "", "");
+        husband.setIssueDepartment(po1);
+        husband.setStudentId("" + (100000 + id));
+        husband.setAddress(address);
+        husband.setUniversity(new University(2L, ""));
+        husband.setStudentId("HH12345");
+
+        //        WIFE
+        Adult wife = new Adult("Петрова", "Вероника", "Алексеевна", LocalDate.of(1998, 3, 12));
+        wife.setPassportSeria("" + (2000 + id));
+        wife.setPassportNumber("" + (200000 + id));
+        wife.setIssueDate(LocalDate.of(2018, 4, 5));
+        PassportOffice po2 = new PassportOffice(2L, "", "");
+        wife.setIssueDepartment(po2);
+        wife.setStudentId("" + (200000 + id));
+        wife.setAddress(address);
+        wife.setUniversity(new University(1L, ""));
+        wife.setStudentId("WW12345");
+
+        //        CHILD1
+        Child child1 = new Child("Петрова", "Ирина", "Викторовна", LocalDate.of(2018, 6, 29));
+        child1.setSertificateNumber("" + (3000 + id));
+        child1.setIssueDate(LocalDate.of(2018, 7, 19));
+        RegisterOffice ro2 = new RegisterOffice(2L, " ", " ");
+        child1.setIssueDepartment(ro2);
+        child1.setAddress(address);
+
+        //        CHILD2
+        Child child2 = new Child("Петров", "Евгений", "Викторович", LocalDate.of(2018, 6, 29));
+        child1.setSertificateNumber("" + (4000 + id));
+        child2.setIssueDate(LocalDate.of(2018, 7, 19));
+        RegisterOffice ro3 = new RegisterOffice(1L, " ", " ");
+        child2.setIssueDepartment(ro3);
+        child2.setAddress(address);
+
+        so.setHusband(husband);
+        so.setWife(wife);
+        so.addChild(child1);
+        so.addChild(child2);
+        return so;
     }
 
     @Before
