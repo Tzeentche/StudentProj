a67721d99bdafd5e5f74fcbd25b147d712af45d6
Tzeentche <ilyasugako87@gmail.com>
2021-02-16 23:40:12 +0300

    Refactored Config class.

--- src/edu/javacourse/studentProj/config/Config.java
+++ src/edu/javacourse/studentProj/config/Config.java
@@ -10,13 +10,14 @@ public class Config {
     public static final String DB_LOGIN = "db.login";
     public static final String DB_PASSWORD = "db.password";
     public static final String DB_LIMIT = "db.limit";
+    public static final String CR_URL = "cr.url";
 
     private static Properties properties = new Properties();
 
     public static String getProperty(String name) {
         if(properties == null) {
 
-            try (InputStream is = Config.class.getClassLoader().getResourceAsStream("edu/javacourse/studentProj/resources/dao.properties")) {
+            try (InputStream is = Config.class.getClassLoader().getResourceAsStream("edu/javacourse/studentProj/resources/config.properties")) {
 
                 properties.load(is);
 
